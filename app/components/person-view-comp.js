fd0dfbd30208412137b9cb8aa13b7c22a885df55
dusanstanojeviccs <dusan.stanojevic.cs@gmail.com>
2016-12-10 12:17:47 -0800

    Revert "Ember issue commit"
    This reverts commit 4f65a47a95202e2c32c601b8fde949e646c01f83.

--- app/components/person-view-comp.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-
-	personChangeObserver: function() {
-		console.log("Person was changed");
-	}.observes("person.id")
-});
--- app/controllers/random-route-two.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-	actions: {
-		setPerson(person) {
-			this.set("person", person);
-		}
-	}
-});
